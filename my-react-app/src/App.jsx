a51ceba1313f5baaa070cd4de1272add05ae1610
neeloo patel <nilusingh945164@gmai.com>
2026-09-10 23:06:07 +0530

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+// import heroImg from './assets/hero.png'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+//import "./App.css";
+
+const Card = () => {
+  return <div>Card component</div>;
+};
+
+const App = () => {
+  return (
+    <div>
+      <h2> Hello world</h2>
+      <Card />
+    </div>
+  );
+};
+
+export default App;
