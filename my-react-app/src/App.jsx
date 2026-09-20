ba7d5e1b7377891bf6a03a1938e8f49dbdf20f11
neeloo patel <nilusingh945164@gmai.com>
2026-09-20 18:31:03 +0530

    todolist

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,3 @@
-// import { useState } from 'react'
-// import heroImg from './assets/hero.png'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-//import "./App.css";
 
 const Card = () => {
   return <div>Card component</div>;
